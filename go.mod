ec1857ab242699c8b006b73cd8cc831ecffbb0d6
Philipp Becker <github@pecebe.de>
2020-05-31 13:22:48 +0200

    First crappy pdf extraction

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Blesmol/pfsct
 
 go 1.14
+
+require github.com/pdfcpu/pdfcpu v0.3.3
--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	pdfcpuapi "github.com/pdfcpu/pdfcpu/pkg/api"
+	"github.com/pdfcpu/pdfcpu/pkg/pdfcpu"
+)
 
 func main() {
-	fmt.Print("Lets start")
+	// extract page 35 from input
+	pdfcpuapi.ExtractPagesFile("scenario.pdf", "test", []string{"35"}, nil)
+	extractedPage := "test/page_35.pdf"
+
+	// add demo watermark do page
+	onTop := true
+	wm, _ := pdfcpu.ParseTextWatermarkDetails("Demo", "", onTop)
+	extracedPageWithWatermark := "test/page_35_wm.pdf"
+	err := pdfcpuapi.AddWatermarksFile(extractedPage, extracedPageWithWatermark, nil, wm, nil)
+	if err != nil {
+		fmt.Printf("Error while adding watermark: %v", err)
+	}
 }
