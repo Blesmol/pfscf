9b9e6a0a6a646ef04adb5088f25df442e5632020
Philipp Becker <github@pecebe.de>
2020-05-31 23:11:14 +0200

    a little bit of structuring...

--- main.go
+++ main.go
@@ -2,22 +2,60 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
 
 	pdfcpuapi "github.com/pdfcpu/pdfcpu/pkg/api"
 	"github.com/pdfcpu/pdfcpu/pkg/pdfcpu"
 )
 
+const input = "scenario1.pdf"
+const output = "test/chronicle1.pdf"
+
+func assert(cond bool, err error) {
+	if cond == false {
+		fmt.Printf("Error is %v\n", err)
+		panic(err)
+	}
+}
+
+func assertNoError(err error) {
+	assert(err == nil, err)
+}
+
+func getTempDir() (name string) {
+	name, err := ioutil.TempDir("", "pfsct")
+	assertNoError(err)
+	return name
+}
+
+func getLastPage(file string) (page string) {
+	numPages, err := pdfcpuapi.PageCountFile(file)
+	assertNoError(err)
+	return strconv.Itoa(numPages)
+}
+
+func getPdfPageExtractionFilename(dir string, page string) (filename string) {
+	localFilename := strings.Join([]string{"page_", page, ".pdf"}, "")
+	return filepath.Join(dir, localFilename)
+}
+
 func main() {
-	// extract page 35 from input
-	pdfcpuapi.ExtractPagesFile("scenario.pdf", "test", []string{"35"}, nil)
-	extractedPage := "test/page_35.pdf"
+	// prepare temporary working dir
+	workDir := getTempDir()
+	defer os.RemoveAll(workDir)
+
+	// extract chronicle page from pdf
+	chroniclePage := getLastPage(input)
+	pdfcpuapi.ExtractPagesFile(input, workDir, []string{chroniclePage}, nil)
+	extractedPage := getPdfPageExtractionFilename(workDir, chroniclePage)
 
 	// add demo watermark do page
 	onTop := true
 	wm, _ := pdfcpu.ParseTextWatermarkDetails("Demo", "", onTop)
-	extracedPageWithWatermark := "test/page_35_wm.pdf"
-	err := pdfcpuapi.AddWatermarksFile(extractedPage, extracedPageWithWatermark, nil, wm, nil)
-	if err != nil {
-		fmt.Printf("Error while adding watermark: %v", err)
-	}
+	err := pdfcpuapi.AddWatermarksFile(extractedPage, output, nil, wm, nil)
+	assertNoError(err)
 }
