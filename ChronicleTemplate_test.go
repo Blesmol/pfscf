2d8d421187d22ce4502e2de36edac17ba251e379
Philipp Becker <github@pecebe.de>
2020-07-04 09:56:18 +0200

    test: apply golang test guidelines

--- ChronicleTemplate_test.go
+++ ChronicleTemplate_test.go
@@ -29,98 +29,109 @@ func getCTfromYamlFile(t *testing.T, filename string) (ct *ChronicleTemplate) {
 	return ct
 }
 
-func Test_NewChronicleTemplate_NoFilename(t *testing.T) {
-	fileToTest := filepath.Join(chronicleTemplateTestDir, "basic.yml")
-	yFile, err := GetYamlFile(fileToTest)
+func TestNewChronicleTemplate(t *testing.T) {
+	t.Run("errors", func(t *testing.T) {
+		t.Run("no filename", func(t *testing.T) {
+			fileToTest := filepath.Join(chronicleTemplateTestDir, "basic.yml")
+			yFile, err := GetYamlFile(fileToTest)
 
-	expectNotNil(t, yFile)
-	expectNoError(t, err)
+			expectNotNil(t, yFile)
+			expectNoError(t, err)
 
-	ct, err := NewChronicleTemplate("", yFile)
-	expectNil(t, ct)
-	expectError(t, err)
+			ct, err := NewChronicleTemplate("", yFile)
+			expectNil(t, ct)
+			expectError(t, err)
+		})
+
+		t.Run("nil file", func(t *testing.T) {
+			ct, err := NewChronicleTemplate("file.yml", nil)
+			expectNil(t, ct)
+			expectError(t, err)
+		})
+
+		t.Run("empty fields", func(t *testing.T) {
+			filenames := []string{"emptyId.yml", "emptyDescription.yml"}
+
+			for _, filename := range filenames {
+				fileToTest := filepath.Join(chronicleTemplateTestDir, filename)
+				yFile, err := GetYamlFile(fileToTest)
+
+				expectNotNil(t, yFile)
+				expectNoError(t, err)
+
+				ct, err := NewChronicleTemplate("foo", yFile)
+				expectNil(t, ct)
+				expectError(t, err)
+			}
+		})
+
+	})
+
+	t.Run("valid", func(t *testing.T) {
+		t.Run("basic valid file", func(t *testing.T) {
+			fileToTest := filepath.Join(chronicleTemplateTestDir, "basic.yml")
+			yFile, err := GetYamlFile(fileToTest)
+
+			expectNotNil(t, yFile)
+			expectNoError(t, err)
+
+			ct, err := NewChronicleTemplate("foo.yml", yFile)
+			expectNotNil(t, ct)
+			expectNoError(t, err)
+
+			expectEqual(t, ct.ID(), "simpleId")
+			expectEqual(t, ct.Description(), "simpleDescription")
+			expectNotSet(t, ct.Inherit())
+			expectTrue(t, strings.HasSuffix(ct.Filename(), "foo.yml"))
+			expectEqual(t, len(ct.content), 0)
+			expectEqual(t, len(ct.presets), 0)
+		})
+
+		t.Run("file with content", func(t *testing.T) {
+			fileToTest := filepath.Join(chronicleTemplateTestDir, "valid.yml")
+			yFile, err := GetYamlFile(fileToTest)
+
+			expectNotNil(t, yFile)
+			expectNoError(t, err)
+
+			ct, err := NewChronicleTemplate("valid.yml", yFile)
+			expectNotNil(t, ct)
+			expectNoError(t, err)
+
+			expectEqual(t, ct.Inherit(), "otherId")
+
+			// content
+			expectEqual(t, len(ct.content), 3)
+			c0, exists := ct.GetContent("c0")
+			expectTrue(t, exists)
+			expectEqual(t, c0.Type(), "textCell")
+			expectEqual(t, c0.X1(), 1.0)
+			c1, exists := ct.GetContent("c1")
+			expectTrue(t, exists)
+			expectEqual(t, c1.Type(), "textCell")
+			expectEqual(t, c1.X1(), 2.0)
+			_, exists = ct.GetContent("p0")
+			expectFalse(t, exists)
+
+			// presets
+			expectEqual(t, len(ct.presets), 3)
+			p0, exists := ct.GetPreset("p0")
+			expectTrue(t, exists)
+			expectEqual(t, p0.Y1(), 10.0)
+			p1, exists := ct.GetPreset("p1")
+			expectTrue(t, exists)
+			expectEqual(t, p1.Y1(), 11.0)
+			_, exists = ct.GetPreset("c0")
+			expectFalse(t, exists)
+		})
+
+	})
+
+	t.Run("", func(t *testing.T) {
+	})
 }
 
-func Test_NewChronicleTemplate_NilFile(t *testing.T) {
-	ct, err := NewChronicleTemplate("file.yml", nil)
-	expectNil(t, ct)
-	expectError(t, err)
-}
-
-func Test_NewChronicleTemplate_EmptyFields(t *testing.T) {
-	filenames := []string{"emptyId.yml", "emptyDescription.yml"}
-
-	for _, filename := range filenames {
-		fileToTest := filepath.Join(chronicleTemplateTestDir, filename)
-		yFile, err := GetYamlFile(fileToTest)
-
-		expectNotNil(t, yFile)
-		expectNoError(t, err)
-
-		ct, err := NewChronicleTemplate("foo", yFile)
-		expectNil(t, ct)
-		expectError(t, err)
-	}
-}
-
-func Test_NewChronicleTemplate_BasicValidFile(t *testing.T) {
-	fileToTest := filepath.Join(chronicleTemplateTestDir, "basic.yml")
-	yFile, err := GetYamlFile(fileToTest)
-
-	expectNotNil(t, yFile)
-	expectNoError(t, err)
-
-	ct, err := NewChronicleTemplate("foo.yml", yFile)
-	expectNotNil(t, ct)
-	expectNoError(t, err)
-
-	expectEqual(t, ct.ID(), "simpleId")
-	expectEqual(t, ct.Description(), "simpleDescription")
-	expectNotSet(t, ct.Inherit())
-	expectTrue(t, strings.HasSuffix(ct.Filename(), "foo.yml"))
-	expectEqual(t, len(ct.content), 0)
-	expectEqual(t, len(ct.presets), 0)
-}
-
-func Test_NewChronicleTemplate_FileWithContent(t *testing.T) {
-	fileToTest := filepath.Join(chronicleTemplateTestDir, "valid.yml")
-	yFile, err := GetYamlFile(fileToTest)
-
-	expectNotNil(t, yFile)
-	expectNoError(t, err)
-
-	ct, err := NewChronicleTemplate("valid.yml", yFile)
-	expectNotNil(t, ct)
-	expectNoError(t, err)
-
-	expectEqual(t, ct.Inherit(), "otherId")
-
-	// content
-	expectEqual(t, len(ct.content), 3)
-	c0, exists := ct.GetContent("c0")
-	expectTrue(t, exists)
-	expectEqual(t, c0.Type(), "textCell")
-	expectEqual(t, c0.X1(), 1.0)
-	c1, exists := ct.GetContent("c1")
-	expectTrue(t, exists)
-	expectEqual(t, c1.Type(), "textCell")
-	expectEqual(t, c1.X1(), 2.0)
-	_, exists = ct.GetContent("p0")
-	expectFalse(t, exists)
-
-	// presets
-	expectEqual(t, len(ct.presets), 3)
-	p0, exists := ct.GetPreset("p0")
-	expectTrue(t, exists)
-	expectEqual(t, p0.Y1(), 10.0)
-	p1, exists := ct.GetPreset("p1")
-	expectTrue(t, exists)
-	expectEqual(t, p1.Y1(), 11.0)
-	_, exists = ct.GetPreset("c0")
-	expectFalse(t, exists)
-}
-
-func Test_GetContentIDs(t *testing.T) {
+func TestGetContentIDs(t *testing.T) {
 	ct := getCTfromYamlFile(t, "valid.yml")
 
 	idList := ct.GetContentIDs(true)
@@ -142,7 +153,7 @@ func Test_GetContentIDs(t *testing.T) {
 	expectEqual(t, idList[2], "c2")
 }
 
-func Test_GetPresetIDs(t *testing.T) {
+func TestGetPresetIDs(t *testing.T) {
 	ct := getCTfromYamlFile(t, "valid.yml")
 
 	idList := ct.GetPresetIDs()
@@ -161,7 +172,7 @@ func Test_GetPresetIDs(t *testing.T) {
 	expectEqual(t, idList[2], "p2")
 }
 
-func Test_InheritFrom(t *testing.T) {
+func TestInheritFrom(t *testing.T) {
 
 	t.Run("inherit from valid", func(t *testing.T) {
 		ctTo := getCTfromYamlFile(t, "inheritTo.yml")
--- YamlFile_test.go
+++ YamlFile_test.go
@@ -16,191 +16,207 @@ func init() {
 	yamlTestDir = filepath.Join(GetExecutableDir(), "testdata", "YamlFile")
 }
 
-func Test_GetYamlFile_NonExistantFile(t *testing.T) {
-	fileToTest := filepath.Join(yamlTestDir, "nonExistantFile.yml")
-	yFile, err := GetYamlFile(fileToTest)
+func TestGetYamlFile(t *testing.T) {
+	t.Run("errors", func(t *testing.T) {
+		t.Run("non-existant file", func(t *testing.T) {
+			fileToTest := filepath.Join(yamlTestDir, "nonExistantFile.yml")
+			yFile, err := GetYamlFile(fileToTest)
 
-	expectNil(t, yFile)
-	expectError(t, err)
+			expectNil(t, yFile)
+			expectError(t, err)
+		})
+
+		t.Run("malformed file", func(t *testing.T) {
+			fileToTest := filepath.Join(yamlTestDir, "malformed.yml")
+			yFile, err := GetYamlFile(fileToTest)
+
+			expectNil(t, yFile)
+			expectError(t, err)
+		})
+
+		t.Run("unknown fields", func(t *testing.T) {
+			fileToTest := filepath.Join(yamlTestDir, "unknownFields.yml")
+			yFile, err := GetYamlFile(fileToTest)
+
+			expectNil(t, yFile)
+			expectError(t, err)
+		})
+
+		t.Run("field type mismatch", func(t *testing.T) {
+			fileToTest := filepath.Join(yamlTestDir, "fieldTypeMismatch.yml")
+			yFile, err := GetYamlFile(fileToTest)
+
+			expectNil(t, yFile)
+			expectError(t, err)
+		})
+	})
+
+	t.Run("valid", func(t *testing.T) {
+		t.Run("empty file", func(t *testing.T) {
+			fileToTest := filepath.Join(yamlTestDir, "empty.yml")
+			yFile, err := GetYamlFile(fileToTest)
+
+			expectNotNil(t, yFile)
+			expectNoError(t, err)
+
+			// empty file => no default section and no content
+			expectNotSet(t, yFile.ID)
+			expectNotSet(t, yFile.Description)
+			expectNotSet(t, yFile.Inherit)
+			expectEqual(t, len(yFile.Presets), 0)
+			expectEqual(t, len(yFile.Content), 0)
+		})
+
+		t.Run("valid file", func(t *testing.T) {
+			fileToTest := filepath.Join(yamlTestDir, "valid.yml")
+			yFile, err := GetYamlFile(fileToTest)
+
+			expectNotNil(t, yFile)
+			expectNoError(t, err)
+
+			expectEqual(t, yFile.ID, "myID")
+			expectEqual(t, yFile.Description, "my Description")
+			expectEqual(t, yFile.Inherit, "myInheritId")
+
+			// presets
+			expectEqual(t, len(yFile.Presets), 2)
+
+			expectKeyExists(t, yFile.Presets, "preset0")
+			p0 := yFile.Presets["preset0"]
+			expectEqual(t, p0.Y1, 150.0)
+			expectEqual(t, p0.Y2, 200.0)
+
+			expectKeyExists(t, yFile.Presets, "preset1")
+			p1 := yFile.Presets["preset1"]
+			expectEqual(t, p1.Font, "my font")
+			expectEqual(t, p1.Fontsize, 10.0)
+
+			// number of entries in content array
+			expectEqual(t, len(yFile.Content), 2)
+
+			expectKeyExists(t, yFile.Content, "myId")
+			c0 := yFile.Content["myId"]
+			expectAllExportedSet(t, c0)
+			expectEqual(t, c0.Type, "my Type")
+			expectEqual(t, c0.Desc, "my Desc")
+			expectEqual(t, c0.X1, 11.0)
+			expectEqual(t, c0.Y1, 12.0)
+			expectEqual(t, c0.X2, 13.0)
+			expectEqual(t, c0.Y2, 14.0)
+			expectEqual(t, c0.XPivot, 15.0)
+			expectEqual(t, c0.Font, "my Font")
+			expectEqual(t, c0.Fontsize, 16.0)
+			expectEqual(t, c0.Align, "my Align")
+			expectEqual(t, c0.Example, "my Example")
+
+			expectKeyExists(t, yFile.Content, "myOtherId")
+			c1 := yFile.Content["myOtherId"]
+			expectAllExportedSet(t, c1)
+			expectEqual(t, c1.Type, "my other type")
+			expectEqual(t, c1.Desc, "my other desc")
+			expectEqual(t, c1.X1, 21.0)
+			expectEqual(t, c1.Y1, 22.0)
+			expectEqual(t, c1.X2, 23.0)
+			expectEqual(t, c1.Y2, 24.0)
+			expectEqual(t, c1.XPivot, 25.0)
+			expectEqual(t, c1.Font, "my other font")
+			expectEqual(t, c1.Fontsize, 26.0)
+			expectEqual(t, c1.Align, "my other align")
+			expectEqual(t, c1.Example, "my other example")
+		})
+
+		t.Run("empty content entry", func(t *testing.T) {
+			fileToTest := filepath.Join(yamlTestDir, "emptyContentEntry.yml")
+			yFile, err := GetYamlFile(fileToTest)
+
+			expectNotNil(t, yFile)
+			expectNoError(t, err)
+
+			expectEqual(t, 1, len(yFile.Content)) // one empty entry is included
+			expectKeyExists(t, yFile.Content, "myId")
+			c0 := yFile.Content["myId"]
+
+			// check that all exported fields in the empty content entry are not set
+			refStruct := reflect.ValueOf(c0)
+			for i := 0; i < refStruct.NumField(); i++ {
+				refField := refStruct.Field(i)
+				if refField.CanAddr() && IsSet(refField.Interface()) {
+					t.Errorf("Expected ContentData field '%v' to be not set, but has value '%v' instead", refStruct.Type().Field(i).Name, refField.Interface())
+				}
+			}
+		})
+	})
 }
 
-func Test_GetYamlFile_MalformedFile(t *testing.T) {
-	fileToTest := filepath.Join(yamlTestDir, "malformed.yml")
-	yFile, err := GetYamlFile(fileToTest)
+func TestGetTemplateFilenamesFromDir(t *testing.T) {
+	t.Run("errors", func(t *testing.T) {
+		t.Run("non-existant dir", func(t *testing.T) {
+			dirToTest := filepath.Join(yamlTestDir, "doesNotExist")
+			fileNames, err := GetTemplateFilenamesFromDir(dirToTest)
 
-	expectNil(t, yFile)
-	expectError(t, err)
+			expectError(t, err)
+			expectNil(t, fileNames)
+		})
+	})
+
+	t.Run("valid", func(t *testing.T) {
+		t.Run("nested dirs", func(t *testing.T) {
+			dirToTest := filepath.Join(yamlTestDir, "nestedDirs")
+			fileNames, err := GetTemplateFilenamesFromDir(dirToTest)
+
+			expectNoError(t, err)
+			expectEqual(t, len(fileNames), 5)
+
+			sort.Strings(fileNames) // for testing purposes, lets sort that list for easier comparison
+
+			expectEqual(t, fileNames[0], filepath.Join(dirToTest, "BaR.YmL"))
+			expectEqual(t, fileNames[1], filepath.Join(dirToTest, "dir1", "foo.yml"))
+			expectEqual(t, fileNames[2], filepath.Join(dirToTest, "dir2", "bar.yml"))
+			expectEqual(t, fileNames[3], filepath.Join(dirToTest, "dir3.yml", "foobar.yml"))
+			expectEqual(t, fileNames[4], filepath.Join(dirToTest, "foo.yml"))
+		})
+	})
 }
 
-func Test_GetYamlFile_ValidFile(t *testing.T) {
-	fileToTest := filepath.Join(yamlTestDir, "valid.yml")
-	yFile, err := GetYamlFile(fileToTest)
+func TestGetTemplateFilesFromDir(t *testing.T) {
+	t.Run("errors", func(t *testing.T) {
+		t.Run("non-existant dir", func(t *testing.T) {
+			dirToTest := filepath.Join(yamlTestDir, "doesNotExist")
+			yFiles, err := GetTemplateFilesFromDir(dirToTest)
 
-	expectNotNil(t, yFile)
-	expectNoError(t, err)
+			expectError(t, err)
+			expectNil(t, yFiles)
+		})
+	})
 
-	expectEqual(t, yFile.ID, "myID")
-	expectEqual(t, yFile.Description, "my Description")
-	expectEqual(t, yFile.Inherit, "myInheritId")
+	t.Run("valid", func(t *testing.T) {
+		dirToTest := filepath.Join(yamlTestDir, "nestedDirs")
+		yFiles, err := GetTemplateFilesFromDir(dirToTest)
 
-	// presets
-	expectEqual(t, len(yFile.Presets), 2)
+		expectNoError(t, err)
+		expectEqual(t, len(yFiles), 5)
 
-	expectKeyExists(t, yFile.Presets, "preset0")
-	p0 := yFile.Presets["preset0"]
-	expectEqual(t, p0.Y1, 150.0)
-	expectEqual(t, p0.Y2, 200.0)
-
-	expectKeyExists(t, yFile.Presets, "preset1")
-	p1 := yFile.Presets["preset1"]
-	expectEqual(t, p1.Font, "my font")
-	expectEqual(t, p1.Fontsize, 10.0)
-
-	// number of entries in content array
-	expectEqual(t, len(yFile.Content), 2)
-
-	expectKeyExists(t, yFile.Content, "myId")
-	c0 := yFile.Content["myId"]
-	expectAllExportedSet(t, c0)
-	expectEqual(t, c0.Type, "my Type")
-	expectEqual(t, c0.Desc, "my Desc")
-	expectEqual(t, c0.X1, 11.0)
-	expectEqual(t, c0.Y1, 12.0)
-	expectEqual(t, c0.X2, 13.0)
-	expectEqual(t, c0.Y2, 14.0)
-	expectEqual(t, c0.XPivot, 15.0)
-	expectEqual(t, c0.Font, "my Font")
-	expectEqual(t, c0.Fontsize, 16.0)
-	expectEqual(t, c0.Align, "my Align")
-	expectEqual(t, c0.Example, "my Example")
-
-	expectKeyExists(t, yFile.Content, "myOtherId")
-	c1 := yFile.Content["myOtherId"]
-	expectAllExportedSet(t, c1)
-	expectEqual(t, c1.Type, "my other type")
-	expectEqual(t, c1.Desc, "my other desc")
-	expectEqual(t, c1.X1, 21.0)
-	expectEqual(t, c1.Y1, 22.0)
-	expectEqual(t, c1.X2, 23.0)
-	expectEqual(t, c1.Y2, 24.0)
-	expectEqual(t, c1.XPivot, 25.0)
-	expectEqual(t, c1.Font, "my other font")
-	expectEqual(t, c1.Fontsize, 26.0)
-	expectEqual(t, c1.Align, "my other align")
-	expectEqual(t, c1.Example, "my other example")
-}
-
-func Test_GetYamlFile_EmptyFile(t *testing.T) {
-	fileToTest := filepath.Join(yamlTestDir, "empty.yml")
-	yFile, err := GetYamlFile(fileToTest)
-
-	expectNotNil(t, yFile)
-	expectNoError(t, err)
-
-	// empty file => no default section and no content
-	expectNotSet(t, yFile.ID)
-	expectNotSet(t, yFile.Description)
-	expectNotSet(t, yFile.Inherit)
-	expectEqual(t, len(yFile.Presets), 0)
-	expectEqual(t, len(yFile.Content), 0)
-}
-
-func Test_GetYamlFile_EmptyContentEntry(t *testing.T) {
-	fileToTest := filepath.Join(yamlTestDir, "emptyContentEntry.yml")
-	yFile, err := GetYamlFile(fileToTest)
-
-	expectNotNil(t, yFile)
-	expectNoError(t, err)
-
-	expectEqual(t, 1, len(yFile.Content)) // one empty entry is included
-	expectKeyExists(t, yFile.Content, "myId")
-	c0 := yFile.Content["myId"]
-
-	// check that all exported fields in the empty content entry are not set
-	refStruct := reflect.ValueOf(c0)
-	for i := 0; i < refStruct.NumField(); i++ {
-		refField := refStruct.Field(i)
-		if refField.CanAddr() && IsSet(refField.Interface()) {
-			t.Errorf("Expected ContentData field '%v' to be not set, but has value '%v' instead", refStruct.Type().Field(i).Name, refField.Interface())
+		// extract filenames for checking completeness
+		var yFilenames []string
+		for yFilename := range yFiles {
+			yFilenames = append(yFilenames, yFilename)
 		}
-	}
-}
+		sort.Strings(yFilenames)
 
-func Test_GetYamlFile_UnknownFields(t *testing.T) {
-	fileToTest := filepath.Join(yamlTestDir, "unknownFields.yml")
-	yFile, err := GetYamlFile(fileToTest)
+		expectEqual(t, yFilenames[0], filepath.Join(dirToTest, "BaR.YmL"))
+		expectEqual(t, yFilenames[1], filepath.Join(dirToTest, "dir1", "foo.yml"))
+		expectEqual(t, yFilenames[2], filepath.Join(dirToTest, "dir2", "bar.yml"))
+		expectEqual(t, yFilenames[3], filepath.Join(dirToTest, "dir3.yml", "foobar.yml"))
+		expectEqual(t, yFilenames[4], filepath.Join(dirToTest, "foo.yml"))
 
-	expectNil(t, yFile)
-	expectError(t, err)
-}
-
-func Test_GetYamlFile_FieldTypeMismatch(t *testing.T) {
-	fileToTest := filepath.Join(yamlTestDir, "fieldTypeMismatch.yml")
-	yFile, err := GetYamlFile(fileToTest)
-
-	expectNil(t, yFile)
-	expectError(t, err)
-}
-
-func Test_GetTemplateFilenamesFromDir_ValidDir(t *testing.T) {
-	dirToTest := filepath.Join(yamlTestDir, "nestedDirs")
-	fileNames, err := GetTemplateFilenamesFromDir(dirToTest)
-
-	expectNoError(t, err)
-	expectEqual(t, len(fileNames), 5)
-
-	sort.Strings(fileNames) // for testing purposes, lets sort that list for easier comparison
-
-	expectEqual(t, fileNames[0], filepath.Join(dirToTest, "BaR.YmL"))
-	expectEqual(t, fileNames[1], filepath.Join(dirToTest, "dir1", "foo.yml"))
-	expectEqual(t, fileNames[2], filepath.Join(dirToTest, "dir2", "bar.yml"))
-	expectEqual(t, fileNames[3], filepath.Join(dirToTest, "dir3.yml", "foobar.yml"))
-	expectEqual(t, fileNames[4], filepath.Join(dirToTest, "foo.yml"))
-}
-
-func Test_GetTemplateFilenamesFromDir_NonExistantDir(t *testing.T) {
-	dirToTest := filepath.Join(yamlTestDir, "doesNotExist")
-	fileNames, err := GetTemplateFilenamesFromDir(dirToTest)
-
-	expectError(t, err)
-	expectNil(t, fileNames)
-}
-
-func Test_GetTemplateFilesFromDir_ValidDir(t *testing.T) {
-	dirToTest := filepath.Join(yamlTestDir, "nestedDirs")
-	yFiles, err := GetTemplateFilesFromDir(dirToTest)
-
-	expectNoError(t, err)
-	expectEqual(t, len(yFiles), 5)
-
-	// extract filenames for checking completeness
-	var yFilenames []string
-	for yFilename := range yFiles {
-		yFilenames = append(yFilenames, yFilename)
-	}
-	sort.Strings(yFilenames)
-
-	expectEqual(t, yFilenames[0], filepath.Join(dirToTest, "BaR.YmL"))
-	expectEqual(t, yFilenames[1], filepath.Join(dirToTest, "dir1", "foo.yml"))
-	expectEqual(t, yFilenames[2], filepath.Join(dirToTest, "dir2", "bar.yml"))
-	expectEqual(t, yFilenames[3], filepath.Join(dirToTest, "dir3.yml", "foobar.yml"))
-	expectEqual(t, yFilenames[4], filepath.Join(dirToTest, "foo.yml"))
-
-	// basic check of the contents of one file
-	foundTestFile := false
-	for yFilename, yFile := range yFiles {
-		if yFilename == filepath.Join(dirToTest, "foo.yml") {
-			expectEqual(t, yFile.ID, "test")
-			foundTestFile = true
+		// basic check of the contents of one file
+		foundTestFile := false
+		for yFilename, yFile := range yFiles {
+			if yFilename == filepath.Join(dirToTest, "foo.yml") {
+				expectEqual(t, yFile.ID, "test")
+				foundTestFile = true
+			}
 		}
-	}
-	expectEqual(t, foundTestFile, true)
-}
-
-func Test_GetTemplateFilesFromDir_NonExistantDir(t *testing.T) {
-	dirToTest := filepath.Join(yamlTestDir, "doesNotExist")
-	yFiles, err := GetTemplateFilesFromDir(dirToTest)
-
-	expectError(t, err)
-	expectNil(t, yFiles)
+		expectEqual(t, foundTestFile, true)
+	})
 }
