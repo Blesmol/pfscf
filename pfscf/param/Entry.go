56761e69a7d41105f35a880747bf341a0fbe7b5f
Philipp Becker <github@pecebe.de>
2020-08-13 16:02:33 +0200

    Add new package 'param'

---
+++ pfscf/param/Entry.go
@@ -0,0 +1,25 @@
+package param
+
+import (
+	"fmt"
+
+	"github.com/Blesmol/pfscf/pfscf/args"
+	"github.com/Blesmol/pfscf/pfscf/utils"
+)
+
+// Entry defines the interface for all param entries
+type Entry interface {
+	ID() string
+	setID(string)
+	Type() string
+	Example() string
+	Description() string
+	deepCopy() Entry
+	isValid() error
+	validateAndProcessArgs(*args.Store) error
+	describe(bool) string
+}
+
+func genericContentUsageExample(id, exampleValue string) (result string) {
+	return fmt.Sprintf("%v=%v", id, utils.QuoteStringIfRequired(exampleValue))
+}
---
+++ pfscf/param/Store_test.go
@@ -0,0 +1,70 @@
+package param
+
+import (
+	"testing"
+
+	test "github.com/Blesmol/pfscf/pfscf/testutils"
+
+	"gopkg.in/yaml.v2"
+)
+
+func TestStore_AddGet(t *testing.T) {
+	store := newStore()
+
+	_, exists := store.Get("id")
+	test.ExpectFalse(t, exists)
+
+	store.add("id1", &textEntry{TheExample: "example1"})
+	store.add("id2", &societyidEntry{TheExample: "example2"})
+
+	entry, exists := store.Get("id1")
+	test.ExpectTrue(t, exists)
+	test.ExpectEqual(t, entry.ID(), "id1")
+	test.ExpectEqual(t, entry.Example(), "example1")
+
+	entry, exists = store.Get("id2")
+	test.ExpectTrue(t, exists)
+	test.ExpectEqual(t, entry.ID(), "id2")
+	test.ExpectEqual(t, entry.Example(), "example2")
+
+	_, exists = store.Get("id")
+	test.ExpectFalse(t, exists)
+}
+
+func TestStore_UnmarshalYAML(t *testing.T) {
+	t.Run("errors", func(t *testing.T) {
+		yamlInput := []byte("id1:\n  type: invalid")
+
+		var store Store
+		err := yaml.Unmarshal(yamlInput, &store)
+
+		test.ExpectError(t, err, "Unknown type")
+	})
+
+	t.Run("valid", func(t *testing.T) {
+		yamlInput := []byte(`
+id1:
+  type: text
+  example: example1
+id2:
+  type: societyid
+  example: example2
+`)
+
+		var store Store
+		err := yaml.Unmarshal(yamlInput, &store)
+
+		test.ExpectNoError(t, err)
+		test.ExpectEqual(t, len(store), 2)
+
+		entry, exists := store.Get("id1")
+		test.ExpectTrue(t, exists)
+		test.ExpectEqual(t, entry.Type(), typeText)
+		test.ExpectEqual(t, entry.Example(), "example1")
+
+		entry, exists = store.Get("id2")
+		test.ExpectTrue(t, exists)
+		test.ExpectEqual(t, entry.Type(), typeSocietyID)
+		test.ExpectEqual(t, entry.Example(), "example2")
+	})
+}
